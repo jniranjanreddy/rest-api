154377d304454647d0e1b42dcb4d081599f3ff7b
Your Name <you@example.com>
2024-04-20 00:53:27 +0530

    mongo-api-working-fine

---
+++ Flask-with-pymongo/Readme.md
@@ -0,0 +1,7 @@
+## https://github.com/Princekrampah
+## https://github.com/Princekrampah/flaskMongoTodoApp
+
+## Flask with Pymongo
+```
+https://www.youtube.com/watch?v=o8jK5enu4L4
+```
---
+++ Flask-with-pymongo/server.py
@@ -0,0 +1,141 @@
+from flask import Flask, request, Response
+import pymongo
+import json
+from pymongo import MongoClient
+from bson import ObjectId
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host="localhost",
+        port=27017,
+        serverSelectionTimeoutMS = 1000
+    )
+    db = mongo.company
+    mongo.server_info() # Trigger exeption if cannot connect to the database
+except: 
+    print("Error connecting to database")
+##########################################
+##Get method
+@app.route('/users', methods=['GET'])
+def get_some_users():
+    try:
+        data = list(db.users.find())
+        print(data)
+        for user in data:
+            user["_id"] = str(user["_id"])
+        return Response(
+            response=json.dumps(data),
+            status=500,
+            mimetype="application/json"
+        )
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response=json.dumps({"message":"Cannot read users"}),
+            status=500,
+            mimetype="application/json"
+        )
+
+##########################################
+## Post Method
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        user = {
+            "name":request.form["name"],
+            "lastname":request.form["lastname"]
+        }
+        dbResponse = db.users.insert_one(user)
+        print(dbResponse.inserted_id)
+        # for attr in dir(dbResponse):
+        #     print(attr)
+        return Response(
+            response=json.dumps(
+                {"message":"user created", 
+                 "id":f"{dbResponse.inserted_id}"
+                }
+            ),
+            status=200,
+            mimetype="application/json"
+         )
+    except Exception as ex:
+        print("*******")
+        print(ex)
+        print("*******")
+#########################################
+## Put Method
+@app.route('/users/<id>', methods=['PATCH'])
+def update_user(id):
+    try:
+        dbResponse = db.users.update_one(
+            {"_id":ObjectId(id)},
+            {"$set":{"name":request.form["name"]}},
+            # {"$set":{"lastname":request.form["lastname"]}}
+        )
+        # print(dbResponse.modified_count)
+        # for attr in dir(dbResponse):
+        #     print(f"*****{attr}*****")
+        if dbResponse.modified_count == 1:
+           return Response(
+               response=json.dumps(
+                   {"message":"user updated", 
+                    "id":f"{id}"
+                   }
+               ),
+               status=200,
+               mimetype="application/json"
+            )
+        return Response(
+            response=json.dumps(
+                {"message":"Nothing to update", 
+                 "id":f"{id}"
+                }
+            ),
+            status=200,
+            mimetype="application/json"
+         )
+    except Exception as ex:
+        print("*******")
+        print(ex)
+        print("*******")
+        return Response(
+            response=json.dumps(
+                {"message":"Cannot update user", 
+                 "id":f"{id}"
+                }
+            ),
+            status=500,
+            mimetype="application/json"
+         )
+#########################################
+## Delete Method
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    try:
+        dbResponse = db.users.delete_one({"_id":ObjectId(id)})
+        # for attr in dir(dbResponse):
+        #     print(f"*****{attr}*****")
+        if dbResponse.deleted_count == 1:
+            return Response(
+                response=json.dumps(
+                    {"message":"user Deleted", 
+                     "id":f"{id}"
+                    }),
+                status=500,
+                mimetype="application/json"
+            )
+        return Response(
+                response=json.dumps(
+                    {"message":"user not found","id":f"{id}"}),status=500,mimetype="application/json")
+    except Exception as ex:
+        print("*******")
+        print(ex)
+        print("*******")
+        return Response(
+            response=json.dumps(
+                {"message":"Sorry Cannot delete user","id":f"{id}"}),status=500,mimetype="application/json")
+######################################
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
